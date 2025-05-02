b526ad947d09a267c5e4f6236f726b6d5484c11d
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2025-05-02 04:59:43 +0300

    ,
    ..

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraAnchor; //точка прив'язки камери
+    //за якою вона рухається, навколо якої обертається та до якої наближається
+    private Vector3 offset;//вектор взаємного розміщення персонажа та камери
+    private InputAction lookAction;//рухи маніпулятора миша
+    private float rotationAngleY;
+    private float rotationAngleX;
+    private float rotationSensitivityY = 10f;
+    private float rotationSensitivityX = 5f;
+
+    private float minVerticalAngle = 10f;
+    private float maxVerticalAngle = 80f;
+    void Start()
+    {
+        offset = cameraAnchor.position - transform.position;
+        lookAction = InputSystem.actions.FindAction("Look");
+        rotationAngleY = 0f;
+        rotationAngleX = transform.eulerAngles.x;
+    }
+
+   
+    void Update()
+    {
+        Vector2 lookValue = Time.deltaTime * lookAction.ReadValue<Vector2>();
+        rotationAngleY += rotationSensitivityY * lookValue.x;
+        rotationAngleX += rotationSensitivityX * lookValue.y;
+
+        if (rotationAngleX < minVerticalAngle)
+        {
+            rotationAngleX = minVerticalAngle;
+        }
+        else if (rotationAngleX > maxVerticalAngle)
+        {
+            rotationAngleX = maxVerticalAngle;
+        }
+
+
+        transform.eulerAngles = new Vector3(rotationAngleX, rotationAngleY, 0f);//самого лише обертання камери недостатньо,
+                                                                     //оскільки вона губить персонажа з поля зору через наявність offset
+                                                                     // --його теж треба повертати разом з поворотом камери
+        transform.position = cameraAnchor.position - //offset - без коррекції на поворот
+         Quaternion.Euler(0f,rotationAngleY,0f) * offset;
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody rb;
+    private InputAction moveAction; 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    void Update()
+    {
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        //rb.AddForce(moveValue.x, 0, moveValue.y); -- прив'язка до світових осей
+        // (напрямів) -- незалежно від повороту камери рух іде вздовж постійних напрямів
+
+        //орієнтація за камерою:
+        Vector3 cameraForward = Camera.main.transform.forward;
+        cameraForward.y = 0f;//прибираємо вертикальну компоненту (проектуємо на горизонтальну площину)
+       
+        if(cameraForward == Vector3.zero) //на випадок якщо камера ідеально згори тоді замінюємо forward -> up 
+        {
+            cameraForward=Camera.main.transform.up;
+        }
+        else
+        {
+            cameraForward.Normalize();
+        }
+        cameraForward.Normalize();//призводимо вектор до одиничного розміру (видовжуємо)
+        
+        Vector3 cameraRight = Camera.main.transform.right;//коригування не потребує ,оскільки має бути завжди горизонтальним 
+        rb.AddForce(moveValue.x * cameraRight + moveValue.y * cameraForward);
+    }
+}
