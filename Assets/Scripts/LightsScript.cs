a772c93d120f0a88c229d689f44b124442348daa
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2025-05-06 12:08:54 +0300

    /
    ///

---
+++ Assets/Scripts/LightsScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class LightsScript : MonoBehaviour
+{
+    private Light[] daylights;
+    private Light[] nightlights;
+    private bool isDay;
+
+    void Start()
+    {
+        daylights = GameObject
+            .FindGameObjectsWithTag("Day")
+            .Select(x => x.GetComponent<Light>())
+            .ToArray();
+        nightlights = GameObject
+           .FindGameObjectsWithTag("Night")
+           .Select(x => x.GetComponent<Light>())
+           .ToArray();
+        isDay = true;
+        SetLights();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            isDay = !isDay;
+            SetLights();
+        }
+    }
+    private void SetLights()
+    {
+        if (isDay)
+        {
+            foreach(Light light in daylights)
+            {
+                light.intensity = 1.0f;
+            }
+            foreach (Light light in nightlights)
+            {
+                light.intensity = 0.0f;
+            }
+            RenderSettings.ambientIntensity = 1.0f;
+            RenderSettings.reflectionIntensity = 1.0f;
+        }
+        else
+        {
+            foreach (Light light in daylights)
+            {
+                light.intensity = 0.0f;
+            }
+            foreach (Light light in nightlights)
+            {
+                light.intensity = 1.0f;
+            }
+            RenderSettings.ambientIntensity= 0.0f;
+            RenderSettings.reflectionIntensity = 0.0f;
+        }
+    }
+}
